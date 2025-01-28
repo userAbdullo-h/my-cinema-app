2255a6d152d11f7f931e5646f97737a9cb71cf17
UserAbdullo <metalturbofergana@gmail.com>
2025-01-28 22:02:17 +0500

    finish random movie section

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,38 +1,59 @@
+import React from 'react'
+import MovieService from '../../services/movie-service'
 import './hero.scss'
 
-const Hero = () => {
-	return (
-		<div className='hero'>
-			<div className='hero__info'>
-				<h2>FIND MOVIES</h2>
-				<h1>Tv Shows and more</h1>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam magni
-					modi voluptas incidunt fuga illo quo recusandae impedit doloribus.
-					Fuga, laboriosam ut natus repudiandae porro nihil dicta nesciunt nam
-					vel!
-				</p>
-				<button className='btn btn-primary'>Details</button>
-			</div>
+class Hero extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			name: null,
+			description: null,
+			thumbnail: null,
+			id: null,
+		}
+		this.movieService = new MovieService()
+		this.getMovie()
+	}
+	getMovie = () => {
+		this.movieService.getRandomMovie().then(res => this.setState(res))
+	}
 
-			<div className='hero__movie'>
-				<img src='/public/image1.svg' alt='Image 1' />
+	render() {
+		const { name, description, id, thumbnail } = this.state
 
-				<div className='hero__movie-descr'>
-					<h2>Madellin</h2>
+		return (
+			<div className='hero'>
+				<div className='hero__info'>
+					<h2>FIND MOVIES</h2>
+					<h1>Tv Shows and more</h1>
 					<p>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo
-						praesentium, fugiat rerum sed sint tempora, odit laudantium
-						obcaecati itaque explicabo facilis et deserunt nesciunt expedita
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+						magni modi voluptas incidunt fuga illo quo recusandae impedit
+						doloribus. Fuga, laboriosam ut natus repudiandae porro nihil dicta
+						nesciunt nam vel!
 					</p>
-					<div>
-						<button className='btn btn-secondary'>Random movie</button>
-						<button className='btn btn-primary'>Details</button>
+					<button className='btn btn-primary'>Details</button>
+				</div>
+
+				<div className='hero__movie'>
+					<img src={thumbnail} alt={name} />
+
+					<div className='hero__movie-descr'>
+						<h2>{name}</h2>
+						<p>
+							{description && description.length >= 200
+								? `${description.slice(0, 200)}...`
+								: description}
+						</p>
+						<div>
+							<button className='btn btn-secondary'>Random movie</button>
+							<button className='btn btn-primary'>Details</button>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Hero
--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,4 +1,9 @@
 class MovieService {
+	_apiBase = 'https://api.themoviedb.org/3'
+	_apiLng = 'language=en-US'
+	_apiKey = 'api_key=0c1bed333af9ff38d57cb9498ea4b10d'
+	_apiImg = 'https://image.tmdb.org/t/p/original'
+
 	getResource = async url => {
 		const response = await fetch(url)
 
@@ -9,7 +14,38 @@ class MovieService {
 		return await response.json()
 	}
 
-	getAllPopular = async () => {}
+	getPopularMovies = async () => {
+		return this.getResource(
+			`${this._apiBase}/movie/popular?${this._apiLng}&${this._apiKey}`
+		)
+	}
 
-	getAllTranding = async () => {}
+	getTrandingMovies = async () => {
+		return this.getResource(
+			`${this._apiBase}/movie/top_rated?${this._apiLng}&${this._apiKey}`
+		)
+	}
+
+	getDetailedMovie = async id => {
+		return this.getResource(
+			`${this._apiBase}/movie/278?${this._apiLng}&${this._apiKey}`
+		)
+	}
+
+	getRandomMovie = async () => {
+		const res = await this.getPopularMovies()
+		const movie = res.results[Math.floor(Math.random() * res.results.length)]
+		return this._transformMovie(movie)
+	}
+
+	_transformMovie = movie => {
+		return {
+			name: movie.original_title,
+			description: movie.overview,
+			thumbnail: `${this._apiImg}${movie.poster_path}`,
+			id: movie.id,
+		}
+	}
 }
+
+export default MovieService
